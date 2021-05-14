adc4ef9bdee1e407fcf74690ab6f650fc23237db
iuriiGav <iurii_gavryliuk@ukr.net>
2021-05-14 13:25:04 +0300

    added btn dropdown trans x to up

---
+++ wp-ig-components/js/global/buttons/btn-dropdown-transform-x-to-up.js
@@ -0,0 +1,8 @@
+const animateBtnDropdownSlideToUp = () => {
+  const btn = document.querySelectorAll(`[data-js-trigger="btn-dropdown-transform-x-to-up"]`).item(0);
+  if (btn !== undefined && btn !== null) {
+    btn.addEventListener("click", () => {
+      btn.classList.toggle("btn-dropdown-transform-x-to-up__arrow--active");
+    });
+  }
+};
