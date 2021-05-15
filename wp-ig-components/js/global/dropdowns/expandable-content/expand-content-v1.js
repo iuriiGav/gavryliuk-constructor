2c8e0f3197daa7a5cbea6c01c71cebe5e37d40c1
iuriiGav <iurii_gavryliuk@ukr.net>
2021-05-15 03:33:24 +0300

    aa

--- wp-ig-components/js/global/dropdowns/expandable-content/expand-content-v1.js
+++ wp-ig-components/js/global/dropdowns/expandable-content/expand-content-v1.js
@@ -1,12 +1,20 @@
-export const expandContentV1 = () => {
-  const expandTrigger = document.querySelectorAll(`[data-js-trigger="expand-content-v1-trigger"]`);
+export const expandContentV1 = (
+  dataJsTrigger = "expand-content-v1-trigger",
+  dataJsCollapse = "expand-content-v1-collapse-neighbours"
+) => {
+  const expandTrigger = document.querySelectorAll(`[data-js-trigger="${dataJsTrigger}"]`);
   const numOfExpandElements = expandTrigger.length;
+  const collapseNeighbours =
+    document.querySelectorAll(`[data-js-collapse="${dataJsCollapse}"]`).item(0) !== null &&
+    document.querySelectorAll(`[data-js-collapse="${dataJsCollapse}"]`).item(0) !== undefined;
   const expandingClass = "expand-content-v1__inner-content--is-open";
   for (let i = 0; i < numOfExpandElements; i++) {
     expandTrigger[i].onclick = function (e) {
-      for (var j = 0; j < numOfExpandElements; j++) {
-        if (this != expandTrigger[j]) {
-          expandTrigger[j].parentNode.classList.remove(expandingClass);
+      if (collapseNeighbours) {
+        for (var j = 0; j < numOfExpandElements; j++) {
+          if (this != expandTrigger[j]) {
+            expandTrigger[j].parentNode.classList.remove(expandingClass);
+          }
         }
       }
       this.parentNode.classList.contains(expandingClass)
