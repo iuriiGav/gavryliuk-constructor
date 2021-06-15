1750974fadb595b60628edc4b41f0c0f23f82f45
iuriiGav <iurii_gavryliuk@ukr.net>
2021-06-16 01:05:50 +0300

    a

--- wp-ig-components/js/global/global-funtions/on-click-outside.js
+++ wp-ig-components/js/global/global-funtions/on-click-outside.js
@@ -1,4 +1,4 @@
-const onClickOutside = function (elClass, callback) {
+export const onClickOutside = function (elClass, callback) {
   const element = Array.from(document.getElementsByClassName(elClass));
   const body = document.getElementsByTagName("body");
   element.map(function (el) {
